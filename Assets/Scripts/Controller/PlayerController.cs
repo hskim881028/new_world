14f7e38e9b17c54645444cb735571589491a833c
hskim <andykim881028@gmail.com>
2021-06-06 23:48:15 +0900

    Add player input system

---
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Controller {
+    public class PlayerController : MonoBehaviour {
+        private Vector2 _direction = Vector2.zero;
+        private float _speed = 2.0f;
+
+        private void Update() {
+            transform.Translate(_direction * (_speed * Time.deltaTime));
+        }
+
+        public void OnMovement(InputValue value) {
+            _direction = value.Get<Vector2>();
+        }
+    }
+}
