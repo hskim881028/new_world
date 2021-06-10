252fbb9e4bf7834e9c75aacbbe0393d935c9ecd1
hskim <andykim881028@gmail.com>
2021-06-11 01:20:42 +0900

    Update stage

--- Assets/Scripts/Manager/Stage.cs
+++ Assets/Scripts/Manager/Stage.cs
@@ -1,5 +1,23 @@
-﻿namespace Manager {
-    public class Stage {
-        
+﻿using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+
+namespace Manager {
+    public class Stage : MonoBehaviour {
+        [SerializeField] private GameObject player;
+
+        private EntityManager _entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+
+        private void Start() {
+            SpawnPlayer();
+        }
+
+        private void SpawnPlayer() {
+            var settings = GameObjectConversionSettings.FromWorld(World.DefaultGameObjectInjectionWorld, null);
+            var entityPlayer = GameObjectConversionUtility.ConvertGameObjectHierarchy(player, settings);
+            var entity = _entityManager.Instantiate(entityPlayer);
+            var startPosition = new Vector3(0, -2, 0);
+            _entityManager.SetComponentData(entity, new Translation { Value = startPosition });
+        }
     }
 }
